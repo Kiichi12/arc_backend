6082518ef55ef0bc67c4d43dc36b304d84e5c1be
Kiichi12 <kirannambiar04@gmail.com>
2025-06-21 21:54:15 +0530

    Add .gitignore

--- folders/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create/', views.create_folder, name="create_folder"),
-    path('delete/<int:id>/', views.delete_folder, name="delete_folder"),
-    path('update/<int:id>/', views.rename_folder, name="rename_folder"),
-    path('get/<int:id>/', views.get_children, name="get_children")          # can add optional query as ?mime_type=<mime_type>
-]
--- reports/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create/', views.create_report, name="create_report"),
-    path('all/', views.get_all_reports, name="get_all_reports"),
-    path('user/', views.get_user_reports, name="get_user_reports")
-]
